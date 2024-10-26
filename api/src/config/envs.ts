4af6a1a8126eeedfeaf729b910ba4eaba974c867
Giovaolmos <127804984+Giovaolmos@users.noreply.github.com>
2024-10-25 21:50:11 -0300

    api

--- api/src/config/envs.ts
+++ api/src/config/envs.ts
@@ -1,8 +1,8 @@
-import "dotenv/config";
+import 'dotenv/config';
 
-export const PORT = process.env.PORT || 3008
-export const DB_PORT = process.env.DB_PORT
-export const DB_HOST = process.env.DB_HOST
-export const DB_USERNAME = process.env.DB_USERNAME
-export const DB_PASSWORD = process.env.DB_PASSWORD
-export const DB_DATABASENAME = process.env.DB_DATABASENAME
+export const PORT = process.env.PORT || 3000;
+export const DB_PORT = process.env.DB_PORT;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_USERNAME = process.env.DB_USERNAME;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_DATABASENAME = process.env.DB_DATABASENAME;
