4fa39d6ba812fc7125f9c162d2a522955461709a
Giovanni Olmos <creatuintro123@gmail.com>
2024-10-31 16:29:42 -0300

    initial commit

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,12 @@
-import { server } from './server';
-import { PORT } from './config/envs';
-import 'reflect-metadata';
-import { AppDataSource } from './config/typeorm';
+import { server } from "./server";
+import { PORT } from "./config/envs";
+import "reflect-metadata";
+import { AppDataSource } from "./config/typeorm";
 
 AppDataSource.initialize().then((res) => {
-  console.log('Connection to the database was successfully');
+  console.log("Connection to the database was successfully");
   server.listen(PORT, () => {
     console.log(`Server listening on  http://localhost:${PORT}`);
   });
 });
+//
